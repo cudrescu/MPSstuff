a8c6ba0a43c94e558c11950ea07c322c9c9f63e3
student <n1ck_nike@yahoo.com>
2014-10-31 13:21:21 +0200

    -a

--- taci.c
+++ taci.c
@@ -5,7 +5,23 @@ int verify(char* ip) {
 	
 }
 
-int main() {
+int main(int argc, char ** argv) {
+	
+	//verificare ca inputul e corect	
+	if(argc < 2){
+		printf("Usagae: ./verify IPv4/IPv6\n");
+		return 0;	
+	}
 
-return 0;	
+	//verificare si afisare rezultat
+	int version = verify(argv[1]);
+	if(version == 4)
+		printf("This is a valid IPv4");
+	if(version == 6)
+		printf("This is a valid IPv6");
+	if(version == -1)
+		printf("You stupid fuck, this is an invalid IPv4/IPv6\n");
+	
+	
+	return 0;	
 }
